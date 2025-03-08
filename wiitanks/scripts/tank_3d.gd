2f7592fbe4593d8ed8df6dfc4104e96387fb2c14
artasmaximus <artas.maximus.a@gmail.com>
2025-03-08 14:13:14 -0500

    add: basic collision detection

--- wiitanks/scripts/tank_3d.gd
+++ wiitanks/scripts/tank_3d.gd
@@ -1,24 +1,22 @@
 extends CharacterBody3D
 
 var player_direction = null
+var speed = 5.0
 
 func _ready() -> void:
-	pass
-
-func _process(_delta: float) -> void:
-	if player_direction != null:
-		var speed = 5 
-		velocity.x = cos(player_direction) * speed
-		velocity.z = sin(player_direction) * speed
-
-		rotation = Vector3(0,player_direction, 0)
-
+    pass
 
 func _physics_process(delta: float) -> void:
-	if player_direction != null:
-		move_and_slide()
+    
+    if player_direction != null:
+        velocity.x = cos(player_direction) * speed
+        velocity.z = sin(player_direction) * speed
+
+        rotation = Vector3(0, player_direction, 0)
+
+        move_and_slide()
 
 func setPlayerDirection(direction):
-	player_direction = direction
-	print(player_direction)
-	return
+    player_direction = direction
+    print(player_direction)
+    return
